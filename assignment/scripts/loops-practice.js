c3591d86189a563bd22c5d979b0d14e1eeb9aee4
Sumeiya Abdi <sumeiya619@gmail.com>
2024-04-13 18:43:36 -0500

    completed loop practce

--- assignment/scripts/loops-practice.js
+++ assignment/scripts/loops-practice.js
@@ -13,15 +13,21 @@ for (let i=0; i<4; i++) {
 // 1.a. TODO: Write a for loop to console.log the numbers from 0 to 5 
 //   - Which part of the example loop do you need to change to do this?
 console.log('count from 0 to 5');
-
+for (let i=0; i<6; i++) {
+  console.log(i);
+}
 // 1.b. TODO: Write a for loop to console.log the numbers from 3 to 5
 //   - Which part of the example loop do you need to change to do this?
 console.log('count from 3 to 5');
-
+for ( let i=3; i<6; i++){
+  console.log(i);
+}
 // 1.c. TODO: Write a for loop to console.log EVEN numbers from 2 to 10
 //   - Which part of the example loop do you need to change to do this?
 console.log('count even numbers from 2 to 10 (2, 4, 6, 8, 10):');
-
+for ( let i=2; i<11; i+= 2){
+  console.log(i);
+}
 // 1.d. (STRETCH) TODO: Write a for loop to do a countdown from 5 to 0
 console.log('STRETCH: countdown from 5 to 0');
 
@@ -32,16 +38,35 @@ let stars = ['Polaris', 'Gacrux', 'Formalhaut', 'Rigel', 'Deneb']
 
 // 2.a. TODO: Write a for of loop to console.log each star in the 'stars' array
 console.log('Some stars:');
-
+for (const star of stars){
+  console.log(star);
+}
 
 // 3. While loops  
 console.log('---- 3. While loop ----');
 
 // 3.a. TODO: Write a while loop to console.log each star in the 'stars' array
 console.log('Some stars using while:');
-
+let index1 = 0
+const max1 = stars.length
+while(index1<stars.length) {
+  const star = stars[index1];
+  console.log(star);
+  index1++;
+}
 // 3.b. TODO: Write a while loop to console.log the numbers from 0 to 5
 console.log('count from 0 to 5');
-
+let index = 0;
+const max = 6;
+while(index<max){
+  console.log('in while loop. index:', index);
+  index++;
+}
 // 3.c. TODO: Write a while loop to console.log the numbers from 10 to 5
 console.log('count backwards from 10 to 5');
+let indexx = 10; 
+const maxx = 4;
+while(indexx>maxx){
+  console.log('in while loop. index:', indexx);
+  indexx--;
+}
